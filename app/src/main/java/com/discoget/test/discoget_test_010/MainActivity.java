8401eaa468bb96c544535425ab83f22300adb999
sawerdeman55 <steven.werdeman@gmail.com>
2016-08-10 16:48:36 -0700

    test of minor changes... not commited correctly...

--- app/src/main/java/com/discoget/test/discoget_test_010/MainActivity.java
+++ app/src/main/java/com/discoget/test/discoget_test_010/MainActivity.java
@@ -2,6 +2,9 @@ package com.discoget.test.discoget_test_010;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    public void AddMessage(View view) {
+
+        //Button btn_One = (Button);
+        TextView txt_msgOne;
+
+        txt_msgOne = (TextView) findViewById(R.id.txt_msgOne);
+
+        String msgOne = "Hi Team!!!";
+
+        txt_msgOne.setText(msgOne);
+
+
+
     }
 }
